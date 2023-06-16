8858bfd5e9bf89276564f02085fed6d34668152d
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-06-16 15:22:02 -0400

    Updated: tailwind styles changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
     'Get started in minutes'
   ];
   return (
-    <div className="hero min-h-[80vh] bg-inherit bg-[url('../assets/images/teacher.jpg')] text-black flex-row">
+    <main className="hero min-h-[80vh] bg-inherit bg-[left_top_-8rem] bg-[url('../assets/images/teacher.jpg')] text-black flex-row">
       <div className="hero-content w-[80%] flex-row justify-start">
         <div className="max-w-lg">
           <h1 className="text-5xl flex-col gap-5">
@@ -28,12 +28,12 @@ const Hero = () => {
               </li>
             ))}
           </ul>
-          <button className="btn btn-info text-white hover:btn-ghost">
+          <button className="text-lg mt-[.1rem] cursor-pointer font-semibold self-middle outline-6 outline outline-blue-500 rounded-lg py-2 px-4 bg-blue-500 text-white hover:text-blue-500 hover:bg-white transition-colors">
             Get Started
           </button>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
