74524ff40e81c7df0085162858b84266b5f7d726
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-06-16 15:22:58 -0400

    Updated: added more sections to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import CallToAction from '@/components/CallToAction';
 import Features from '@/components/Features';
 import Hero from '@/components/Hero';
 import NavigationBar from '@/components/NavigationBar';
+import ReviewHeader from '@/components/ReviewHeader';
+import Reviews from '@/components/Reviews';
 import Image from 'next/image';
 
 export default function Home() {
@@ -8,6 +11,9 @@ export default function Home() {
     <>
       <Hero />
       <Features />
+      <ReviewHeader />
+      <Reviews />
+      <CallToAction />
     </>
   );
 }
