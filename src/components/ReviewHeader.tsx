91c323596bee4b3c0d0740c12adf5c8b994b90fe
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-06-16 15:21:27 -0400

    Initial commit

---
+++ src/components/ReviewHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ReviewHeader = () => {
+  return (
+    <section className="hero py-10 bg-blue-500 flex justify-center ">
+      <h2 className="text-blue-50 text-5xl">
+        See what our customers have to say!
+      </h2>
+    </section>
+  );
+};
+
+export default ReviewHeader;
