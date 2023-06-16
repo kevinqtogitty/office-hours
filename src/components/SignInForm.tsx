9ad44487092f30db470aadf0f39b34e5e2640725
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-06-16 15:21:12 -0400

    Initial commit

---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { signInUser } from '@/firebase/auth';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+interface Props {
+  signInActive: boolean;
+}
+
+const SignInForm = ({ signInActive }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+  const onSubmit = (data: { email: string; password: string }) => {
+    signInUser(data.email, data.password);
+  };
+
+  const sharedUtilClasses =
+    'bg-white text-black h-12 rounded-md px-2 focus:outline-4 focus:border-none border-2 border-b-blue-500bg-white text-black border-2 border-b-blue-500';
+
+  return (
+    <form
+      action="submit"
+      onSubmit={handleSubmit(onSubmit)}
+      className={`text-black ${
+        signInActive ? 'block' : 'hidden'
+      } flex flex-col items-center gap-4`}
+    >
+      <label hidden htmlFor="email">
+        Email
+      </label>
+      <input
+        {...register('email', { required: true })}
+        id="email"
+        type="email"
+        placeholder="name@example.com"
+        className={sharedUtilClasses}
+      />
+      {errors.email && <span className="text-red-600">Email is required</span>}
+      <label hidden htmlFor="password">
+        Password
+      </label>
+      <input
+        {...register('password', { required: true })}
+        id="password"
+        type="password"
+        className={sharedUtilClasses}
+      />
+      {errors.password && (
+        <span className="text-red-600">Password is required</span>
+      )}
+      {/* <input type="submit" /> */}
+      <button type="submit" className="btn btn-secondary">
+        login
+      </button>
+    </form>
+  );
+};
+
+export default SignInForm;
