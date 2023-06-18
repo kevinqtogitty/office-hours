72b5181cff91b7dbb9055377447c833afd6085a1
Kevin To <kevinto@Kevins-MacBook-Air-2.local>
2023-06-18 13:49:32 -0400

    Update: added logout button and rerouting

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+'use client';
+import { logoutUser } from '@/firebase/auth';
+import { auth } from '@/firebase/firebaseInit';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+  const router = useRouter();
 
+  useEffect(() => {
+    if (!auth.currentUser) router.push('/login');
+    return;
+  }, []);
+
+  const handleLogout = async () => {
+    await logoutUser();
+    if (!auth.currentUser) router.push('/login');
+  };
+
+  return (
+    <div>
+      {auth.currentUser?.email}
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
 export default Dashboard;
