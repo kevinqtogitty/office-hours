0ab637340cf5245126ac6254774064d950ee9a6a
Kevin To <kevinto@Kevins-MacBook-Air-2.local>
2023-06-19 21:42:19 -0400

    Update: added fetch user on mount

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,46 @@
 'use client';
 import { logoutUser } from '@/firebase/auth';
-import { auth } from '@/firebase/firebaseInit';
+import { auth, db } from '@/firebase/firebaseInit';
 import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { DocumentData, doc, getDoc } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const Dashboard = () => {
+  const [loggedInUser, setLoggedInUser] = useState<DocumentData | undefined>(
+    undefined
+  );
   const router = useRouter();
 
   useEffect(() => {
-    if (!auth.currentUser) router.push('/login');
-    return;
+    fetchUser();
   }, []);
+
+  onAuthStateChanged(auth, () => {
+    if (!auth.currentUser) router.push('/login');
+  });
+
+  const fetchUser = async () => {
+    try {
+      const docRef = doc(db, 'users', `${auth.currentUser?.uid}`);
+      setLoggedInUser((await getDoc(docRef)).data());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleLogout = async () => {
     await logoutUser();
-    if (!auth.currentUser) router.push('/login');
   };
 
   return (
-    <div>
+    <main>
+      <section>
+        <h1>Welcome back, {loggedInUser?.username}</h1>
+        <button onClick={handleLogout}>Logout</button>
+      </section>
       {auth.currentUser?.email}
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    </main>
   );
 };
 export default Dashboard;
