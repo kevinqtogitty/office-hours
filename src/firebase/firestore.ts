67df53970f182cc3eaa878f00a01bb460769a050
Kevin To <kevinto@Kevins-MacBook-Air-2.local>
2023-06-19 21:36:26 -0400

    Update: switched to setDoc from addDoc

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,22 +1,13 @@
-import { collection, addDoc } from 'firebase/firestore';
-import { db } from './firebaseInit';
+import { doc, setDoc } from 'firebase/firestore';
+import { auth, db } from './firebaseInit';
 
-const addNewUserToDb = async (
-  firstName: string,
-  lastName: string,
-  email: string,
-  uid: string
-) => {
+const addNewUserToDb = async (username: string, email: string, uid: string) => {
   try {
-    console.log('adding to db');
-    console.log(firstName, lastName, uid, email);
-    const docRef = await addDoc(collection(db, 'users'), {
-      firstName,
-      lastName,
+    await setDoc(doc(db, 'users', auth.currentUser!.uid), {
+      username,
       uid,
       email
     });
-    console.log('Document written with ID: ', docRef.id);
   } catch (e) {
     console.error('Error adding document: ', e);
   }
